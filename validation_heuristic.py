30838ddd68ff23e9e214cf0ef7b269a0554b60aa
Gianluca Graziadei <graziadei.gianluca@gmail.com>
2024-06-08 13:43:24 +0200

    fragmentation analysis

--- validation_heuristic.py
+++ validation_heuristic.py
@@ -98,18 +98,18 @@ with open(f'{args.folder}/network.json', 'r') as f:
 
                 # for optical links fragmentation
                 
-                if count % 400 == 0:
-                    for link in optical_links:
-                        from pandas import DataFrame
-                        df = link.fragmentation()
-                        df.to_excel(f'{args.folder}/fragmentation_{link.get_id()}.xlsx')
-                        import matplotlib.pyplot as plt
-                        # draw boxplot
-                        df.boxplot(column='size', by='category')
-                        plt.title('Contiguos available time')
-                        plt.ylabel('Available time (us)')
-                        plt.xlabel('Cycle section')
-                        plt.savefig(f'{args.folder}/fragmentation_{link.get_id()}.png')
+            if count % 400 == 0:
+                for link in optical_links:
+                    from pandas import DataFrame
+                    df = link.fragmentation()
+                    df.to_excel(f'{args.folder}/fragmentation_{link.get_id()}.xlsx')
+                    import matplotlib.pyplot as plt
+                    # draw boxplot
+                    df.boxplot(column='size', by='category')
+                    plt.title('Contiguos available time')
+                    plt.ylabel('Available time (us)')
+                    plt.xlabel('Cycle section')
+                    plt.savefig(f'{args.folder}/fragmentation_{link.get_id()}.png')
             count += 1
                               
 # save stats in pickle file
