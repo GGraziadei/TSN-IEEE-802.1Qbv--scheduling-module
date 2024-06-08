e0a1f5aee990df457ef19f710c0d3a4585138312
Gianluca Graziadei <graziadei.gianluca@gmail.com>
2024-06-08 14:01:23 +0200

    Fragmeentation analysis

--- validation_heuristic.py
+++ validation_heuristic.py
@@ -75,17 +75,6 @@ with open(f'{args.folder}/network.json', 'r') as f:
         if not os.path.exists(f'{args.folder}/fragmentation'):
             os.makedirs(f'{args.folder}/fragmentation')
 
-        for link in optical_links:
-            from pandas import DataFrame
-            df = link.fragmentation()
-            df.to_excel(f'{args.folder}/fragmentation/fragmentation_{link.get_id()}_c{count}.xlsx')
-            import matplotlib.pyplot as plt
-            # draw boxplot
-            df.boxplot(column='size', by='category')
-            plt.title('Contiguos available time')
-            plt.ylabel('Available time (us)')
-            plt.xlabel('Cycle section')
-            plt.savefig(f'{args.folder}/fragmentation/fragmentation_{link.get_id()}_c{count}.png')
 
         for flow in flows:
             sol = instance.new_request(flow)
@@ -109,7 +98,7 @@ with open(f'{args.folder}/network.json', 'r') as f:
                     plt.title('Contiguos available time')
                     plt.ylabel('Available time (us)')
                     plt.xlabel('Cycle section')
-                    plt.savefig(f'{args.folder}/fragmentation_{link.get_id()}.png')
+                    plt.savefig(f'{args.folder}/fragmentation/fragmentation_link{link.get_id()}_request{count}.png')
             count += 1
                               
 # save stats in pickle file
